3dbc2e69c3a1f6be56c3adb9cb72a33f71b45f6e
Daewoong <kodw4284@gmail.com>
2020-03-20 00:00:45 +0900

    Add composeWithDevTools

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from '@/modules';
 import './index.css';
 import App from './App';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
   <Provider store={store}>
