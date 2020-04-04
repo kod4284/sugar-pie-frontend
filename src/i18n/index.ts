0ef976477b0eefb6270730a809e626681fd6325e
Daewoong <kodw4284@gmail.com>
2020-04-04 22:20:56 +0900

    Apply i18n

---
+++ src/i18n/index.ts
@@ -0,0 +1,38 @@
+import i18n from 'i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import { initReactI18next } from 'react-i18next';
+import en from '@/i18n/locale/en';
+import ko from '@/i18n/locale/ko';
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    // we init with resources
+    resources: {
+      en: {
+        translations: {
+          ...en as object,
+        },
+      },
+      ko: {
+        translations: {
+          ...ko as object,
+        },
+      },
+    },
+    fallbackLng: 'en',
+    debug: true,
+
+    // have a common namespace used around the full app
+    ns: ['translations'],
+    defaultNS: 'translations',
+
+    keySeparator: false, // we use content as keys
+
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
---
+++ src/i18n/locale/ko.ts
@@ -0,0 +1,3 @@
+export default {
+  'sample.test': '테스트',
+};
