85598cefb3cc16c1c6e13c269acd5db08a7a6a84
Daewoong <kodw4284@gmail.com>
2020-04-05 23:59:44 +0900

    Add component  AuthForm.tsx

---
+++ src/components/auth/AuthForm.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const AuthFormBlock = styled.div``;
+
+function AuthForm() {
+  return (
+    <AuthFormBlock>
+      AuthForm
+    </AuthFormBlock>
+  );
+}
+
+export default AuthForm;
