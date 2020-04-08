67ae25a49b475ef127c9d7fd36726273959e4831
Daewoong <kodw4284@gmail.com>
2020-04-08 22:35:29 +0900

    Compose AuthFrom.tsx with its i18ns

--- src/i18n/locale/en.ts
+++ src/i18n/locale/en.ts
@@ -1,3 +1,7 @@
 export default {
+  login: 'Login',
+  register: 'Reg',
+  username: 'username',
+  password: 'password',
   'main.title': 'SUGAR PIE',
 };
--- src/i18n/locale/ko.ts
+++ src/i18n/locale/ko.ts
@@ -1,3 +1,7 @@
 export default {
+  login: '로그인',
+  register: '회원가입',
+  username: '아이디',
+  password: '비밀번호',
   'main.title': 'SUGAR PIE',
 };
