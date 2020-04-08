fa0d1854c57dd4ee7d95503f1a046367ec731a64
Daewoong <kodw4284@gmail.com>
2020-04-08 23:18:05 +0900

    Add style option if fullWidth prop exists

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
-import styled from 'styled-components';
-import palette from '@/lib/styles/palette';
+import styled, { css } from 'styled-components';
 import { Button as AntdButton } from 'antd';
 
-const StyledButton = styled(AntdButton)`
+const StyledButton = styled<any>(AntdButton)`
   border-radius: 4px;
   font-weight: bold;
+  font-size: 1rem;
+  padding: 0.25rem 1rem;
+  color: white;
+
+  ${props => props.fullWidth
+    && css`
+      padding-top: 0.75rem;
+      padding-bottom: 2.2rem;
+      width: 100%;
+      font-size: 1.125rem;
+    `}
 `;
+
 
 function Button(props: any) {
   return (
     <div>
-      <StyledButton {...props} />
+      <StyledButton type="primary" {...props} />
     </div>
   )
 }
