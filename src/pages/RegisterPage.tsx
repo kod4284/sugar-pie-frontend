c8fc0b758804ebf7ea954ebfda2e355016a1f778
Daewoong <kodw4284@gmail.com>
2020-04-08 23:16:32 +0900

    Compose RegisterPage.tsx

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import AuthTemplate from '@/components/auth/AuthTemplate';
+import AuthForm from '@/components/auth/AuthForm';
 
 function RegisterPage() {
   return (
-    <div>
-      Register Page
-    </div>
+    <AuthTemplate>
+      <AuthForm type="register" />
+    </AuthTemplate>
   );
 }
 
