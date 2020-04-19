7da616207b849e4a81629853e52a26f570679d77
Daewoong <kodw4284@gmail.com>
2020-04-19 22:32:26 +0900

    Add i18n messages

--- src/i18n/locale/en.ts
+++ src/i18n/locale/en.ts
@@ -5,6 +5,9 @@ export default {
   register: 'Register',
   username: 'username',
   password: 'password',
+  'username.required': 'Username is required',
+  'password.required': 'Password is required',
+  'password.confirm.required': 'Password confirm is required',
   'confirm.password': 'Confirmed password',
   'main.title': 'SUGAR PIE',
 };
--- src/i18n/locale/ko.ts
+++ src/i18n/locale/ko.ts
@@ -5,6 +5,9 @@ export default {
   register: '회원가입',
   username: '아이디',
   password: '비밀번호',
+  'username.required': '아이디를 입력해주세요',
+  'password.required': '비밀번호를 입력해주세요',
+  'password.confirm.required': '비밀번호를 다시 입력해주세요',
   'confirm.password': '비밀번호 확인',
   'main.title': 'SUGAR PIE',
 };
