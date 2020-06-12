ea7c5c1cc5c301b1e6c96e8b7a4a0c151f4fcd14
Daewoong <kodw4284@gmail.com>
2020-06-12 16:02:07 +0900

    Update reducer

--- src/modules/auth/actions.ts
+++ src/modules/auth/actions.ts
@@ -1,8 +1,17 @@
 import { createAction } from 'typesafe-actions';
-import { FormField, State } from '@/modules/auth/types';
+
+import { FormField, State, LoginForm } from '@/modules/auth/types';
 
 export const CHANGE_FIELD = 'auth/CHANGE_FIELD';
 export const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
+
+export const REGISTER = 'auth/REGISTER';
+export const REGISTER_SUCCESS = 'auth/REGISTER_SUCCESS';
+export const REGISTER_FAILURE = 'auth/REGISTER_FAILURE';
+
+export const LOGIN = 'auth/LOGIN';
+export const LOGIN_SUCCESS = 'auth/LOGIN_SUCCESS';
+export const LOGIN_FAILURE = 'auth/LOGIN_FAILURE';
 
 export const changeField = createAction(
   CHANGE_FIELD,
@@ -13,5 +22,23 @@ export const changeField = createAction(
   }),
 )<FormField>();
 
-export const initializeForm = createAction(INITIALIZE_FORM, (form) => form)
-<keyof State>();
+export const initializeForm = createAction(
+  INITIALIZE_FORM,
+  (form: keyof State) => form,
+)<keyof State>();
+
+export const register = createAction(
+  REGISTER,
+  ({ username, password }: LoginForm) => ({
+    username,
+    password,
+  }),
+)<LoginForm>();
+
+export const login = createAction(
+  LOGIN,
+  ({ username, password }: LoginForm) => ({
+    username,
+    password,
+  }),
+)<LoginForm>();
--- src/modules/auth/reducer.ts
+++ src/modules/auth/reducer.ts
@@ -2,7 +2,10 @@ import { createReducer } from 'typesafe-actions';
 import { produce } from 'immer';
 
 import { State, AuthAction } from '@/modules/auth/types';
-import { CHANGE_FIELD, INITIALIZE_FORM } from '@/modules/auth/actions';
+import {
+  CHANGE_FIELD,
+  INITIALIZE_FORM,
+} from '@/modules/auth/actions';
 
 export const initialState: State = {
   register: {
@@ -14,17 +17,23 @@ export const initialState: State = {
     username: '',
     password: '',
   },
+  auth: null,
+  authError: null,
 };
 
 const auth = createReducer<State, AuthAction>(initialState, {
   [CHANGE_FIELD]: (state, { payload: { form, key, value } }) =>
-    produce(state, draft => {
+    produce(state, (draft) => {
+      if (form === 'auth' || form === 'authError') {
+        return;
+      }
       const assignedKey = initialState[form];
       draft[form][key as keyof typeof assignedKey] = value;
     }),
   [INITIALIZE_FORM]: (state, { payload: form }) => ({
     ...state,
     [form]: initialState[form],
+    authError: null, // Note:  폼 전환시 인증 에러 초기화
   }),
 });
 
