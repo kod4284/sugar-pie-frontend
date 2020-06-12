1cd6baa9937c13f91bd82ec236ce4f596c09ccee
Daewoong <kodw4284@gmail.com>
2020-06-12 15:32:05 +0900

    Remove useless codes

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -2,19 +2,17 @@ import { combineReducers } from 'redux';
 import { all } from 'redux-saga/effects';
 
 import auth from '@/modules/auth/reducer';
-import counter, { counterSaga } from '@/modules/counter';
-import sample, { sampleSaga } from '@/modules/sample';
 import loading from '@/modules/loading';
+
+import { authSaga } from '@/modules/auth/sagas';
 
 const rootReducer = combineReducers({
   auth,
-  counter,
-  sample,
   loading,
 });
 
 export function* rootSaga() {
-  yield all([counterSaga(), sampleSaga()]);
+  yield all([authSaga()]);
 }
 
 export default rootReducer;
